d43bb20389063924337254d41af582539548875a
Artem Zhitkov <aptem_deadly@mail.ru>
2024-11-09 22:46:54 +0500

    fixes

--- src/search_vacancies.py
+++ src/search_vacancies.py
@@ -9,17 +9,17 @@ class SearchVacancies(Api):
     """Класс для поиска вакансий с помощью API"""
 
     def __init__(self) -> None:
-        self.url = "https://api.hh.ru/vacancies"
-        self.headers = {"User-Agent": "HH-User-Agent"}
-        self.params = {"text": "", "page": 0, "per_page": 100}
-        self.vacancies: list[Any] = []
+        self.__url = "https://api.hh.ru/vacancies"
+        self.__headers = {"User-Agent": "HH-User-Agent"}
+        self.__params = {"text": "", "page": 0, "per_page": 100}
+        self.__vacancies: list[Any] = []
 
     def load_vacancies(self, keyword) -> Any:
-        self.params["text"] = keyword
-        while self.params.get("page") != 20:
-            response = requests.get(self.url, headers=self.headers, params=self.params)
+        self.__params["text"] = keyword
+        while self.__params.get("page") != 20:
+            response = requests.get(self.__url, headers=self.__headers, params=self.__params)
             if response.status_code == 200:
                 vacancies = response.json()["items"]
-                self.vacancies.extend(vacancies)
-                self.params["page"] += 1
-        return self.vacancies
+                self.__vacancies.extend(vacancies)
+                self.__params["page"] += 1
+        return self.__vacancies
