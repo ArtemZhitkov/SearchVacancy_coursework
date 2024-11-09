77f9a133eb905118c4ad49c77ae91ac817db1052
Artem Zhitkov <aptem_deadly@mail.ru>
2024-11-09 22:31:08 +0500

    update

--- tests/conftest.py
+++ tests/conftest.py
@@ -25,3 +25,18 @@ def json_data():
     with open(abs_file_path, 'r', encoding="utf-8") as file:
         data = json.load(file)
         return data
+
+
+@pytest.fixture
+def list_vacancies():
+    return [
+        Vacancy("Фронтенд разработчик", "https://hh.ru/vacancy/109570768", "Ташкент", 2000000,
+                "Знания HTML, CSS, JS, Jquery."),
+        Vacancy("React Front-End Developer (Frontend разработчик)", "https://hh.ru/vacancy/109792373",
+                "Ташкент", 10000,
+                "Базовые знания HTML / CSS / JavaScript. React / React Native. English (на уровне понимания технических текстов). Русский язык (для общения). "),
+        Vacancy("Backend-разработчик", "https://hh.ru/vacancy/110131426",
+                "Ташкент", 40000,
+                "Уверенное владение языком Golang. -Знакомство с Docker. -Понимание CI/CD и облачных платформ (AWS, GCP или Azure). -Отличные навыки решения...")
+    ]
+
